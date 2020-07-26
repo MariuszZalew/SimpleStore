49aa54575cb67ec25eee0d6b2a5a3de663a0a7cb
Mariusz <m.zalew.poczta@gmail.com>
2020-07-26 17:02:05 +0200

    add store component

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+
+@Component({
+    selector: "store",
+    templateUrl: "store.component.html"
+})
+
+export class StoreComponent {
+    
+    constructor(private reposiory: ProductRepository) {}
+
+    get products(): Product[] {
+        return this.reposiory.gerProducts();
+    }
+
+    get categories(): string[] {
+        return this.reposiory.getCategories();
+    }
+
+}
