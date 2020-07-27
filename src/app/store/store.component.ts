9f07305d738c6be5e309afbbba42acb1ee6915e6
Mariusz <m.zalew.poczta@gmail.com>
2020-07-27 06:47:47 +0200

    add category selection

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,15 +8,21 @@ import { ProductRepository } from "../model/product.repository";
 })
 
 export class StoreComponent {
+    public selectedCategory = null;
     
     constructor(private reposiory: ProductRepository) {}
 
     get products(): Product[] {
-        return this.reposiory.gerProducts();
+        return this.reposiory.gerProducts(this.selectedCategory);
+        // return this.reposiory.gerProducts();
     }
 
     get categories(): string[] {
         return this.reposiory.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
     }
 
 }
