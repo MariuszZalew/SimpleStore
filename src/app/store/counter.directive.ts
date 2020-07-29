d20321c491882a26f739c7acf04c2a29cd2418af
Mariusz <m.zalew.poczta@gmail.com>
2020-07-29 09:59:07 +0200

    add routing

--- src/app/store/counter.directive.ts
+++ src/app/store/counter.directive.ts
@@ -14,8 +14,7 @@ export class CounterDirective {
     ngOnChanges(changes: SimpleChanges) {
         this.container.clear();
         for(let i = 0; i < this.counter; i++) {
-            this.container.createEmbeddedView(this.template, 
-                                              new CounterDirectiveContext(i + 1));
+            this.container.createEmbeddedView(this.template, new CounterDirectiveContext(i + 1));
         }
     }
 }
