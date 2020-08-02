aaf371936aaa4ccd29deea183eef48892e7fa3a9
Mariusz <m.zalew.poczta@gmail.com>
2020-08-02 22:52:25 +0200

    add rest config

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,9 +4,13 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { OrderRepository } from "./order.repository";
 import { Order } from "./order.model";
+import { HttpClientModule } from "@angular/common/http";
+import { RestDataSource } from './rest.datasource';
 
 @NgModule({
-    providers: [ ProductRepository, StaticDataSource, Cart, Order, OrderRepository ]
+    imports: [ HttpClientModule ],
+    providers: [ ProductRepository, StaticDataSource, Cart, Order, OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource }]
 })
 
 export class ModelModule { }
